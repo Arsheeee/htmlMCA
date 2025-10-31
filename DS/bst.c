2cffd291aa08ec020bcfcce31a6e4176324d370d
Aswinar01 <aswinar2k03@gmail.com>
2025-10-31 11:43:59 +0530

    bst

--- DS/bst.c
+++ DS/bst.c
@@ -9,25 +9,71 @@ struct node {
     int key;
 };
 
-struct node *root;
+struct node *root=NULL;
 
-void insert(int key)
+struct node *createnode(int value)
 {
-    struct node *temp=root;
-    if(root==NULL)
-    {
-        root=key;
-    }
+    struct node *newnode=(struct node *)malloc(sizeof(struct node));
 
-    else{
-        if(root>key)
-        {
-            while(root->leftlink!=NULL)
-            {
-                r
-            }
-        }
-    }
+    newnode->key=value;
+    newnode->leftlink=NULL;
+    newnode->rightlink=NULL;
+
+    return newnode;
+}
+
+void insert(int value)
+{
+   struct node  *newnodee=createnode(value);
+   struct node *temp=root;
+   struct node *parent=temp;
+
+   if(root==NULL)
+   {
+    root=newnodee;
+   }
+   else{
+    while(temp!=NULL)
+   {
+     parent=temp;
+
+     if(temp->key > value)
+     {
+        temp=temp->leftlink;
+     }
+     else{
+        temp=temp->rightlink;
+     }
+   }
+
+   if(parent->key > value)
+   {
+    parent->leftlink=newnodee;
+   }
+   else{
+    parent->rightlink=newnodee;
+   }
+   }
+}
+
+void preorder(struct node * root)
+{
+  
+}
+
+
+void inorder(struct node *root)
+{
+  if(root!=NULL)
+  {
+    inorder(root->leftlink);
+    printf("%d",root->key);
+    inorder(root->rightlink);
+  }
+}
+
+void postorder(struct node *root)
+{
 
 }
 
@@ -35,23 +81,39 @@ void insert(int key)
 int main()
 {
     int choice;
-    int key;
+    int value;
     while(1)
     {
-     printf("1.Insert 2.display");
-     printf("enter your choice");
+     printf("\n 1.Insert 2.Preorder 3.Inorder 4.Postorder 5.Exit \n");
+     printf("enter your choice : ");
      scanf("%d",&choice);
 
      switch(choice)
      {
         case 1:
          printf("enter the value to be inserted : ");
-         scanf("%d",&key);
-         insert(key);
+         scanf("%d",&value);
+         insert(value);
          break;
 
          case 2:
-         
+         printf("Preorder traversal \n");
+         preorder(root);
+         break;
+
+         case 3:
+         printf("Inorder traversal \n");
+         inorder(root);
+         break;
+
+         case 4:
+         printf("Postorder traversal \n");
+         postorder(root);
+         break;
+
+         case 5:
+         printf("Exiting...\n");
+         exit(0);
 
         default:
           printf("invalid choice");
