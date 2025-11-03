596f37670d29b2d9b03f4bd7feffab529f9d7551
Aswinar01 <aswinar2k03@gmail.com>
2025-11-03 15:45:58 +0530

    sqlconnection

---
+++ Php/database.php
@@ -0,0 +1,17 @@
+<html>
+    <head></head>
+    <body>
+        <form action="database2.php" method="get">
+        Enter the RollNumber : <input type="text" name="rollno"><br><br>
+        Enter the Name : <input type="text" name="name"><br><br>
+        Gender : 
+        <input type ="radio" name = "gender" value = "Male">Male
+        <input type ="radio" name = "gender" value = "Female">Female<br><br>
+        Mark 1 <input type = "text" name="marka"><br><br>
+        Mark 2 <input type = "text" name= "markb"><br><br>
+        <input type = "submit" name= "sub">
+        <input type = "reset" name= "res">
+        
+        </form>
+    </body>
+</html>
---
+++ Php/database2.php
@@ -0,0 +1,51 @@
+<?php
+$rollno = (int)$_GET["rollno"];
+$name = $_GET["name"];
+$gender = $_GET["gender"];
+$mark1 = (int)$_GET["marka"];
+$mark2 = (int)$_GET["markb"];
+$Total = (int) $mark1 + $mark2;
+
+
+
+$con=mysqli_connect("localhost","root","","arshi");
+if($con)
+    echo " connection success";
+else
+    echo "cant be connected";
+
+
+
+$sqlfetch="SELECT * from student";
+
+$result=mysqli_query($con,$sqlfetch);
+
+if(mysqli_num_rows($result)>=1)
+    while($row=mysqli_fetch_assoc($result))
+{
+    // echo "Rollno :" .$row["rollno"];
+//     echo "Name :" .$row["name"];
+//     echo "Rollno :" .$row["gender"];
+//     echo "Rollno :" .$row["mark1"];
+//     echo "Rollno :" .$row["mark2"];
+//     echo "Rollno :" .$row["total"];
+if($row["rollno"]==$rollno)
+{
+
+    echo '<script>alert("Student already exists....Press OK to return to the preveus page");document.location="database.php"</script>';
+    
+    }
+ else{
+    $query = "INSERT INTO student values('$rollno', '$name','$gender','$mark1','$mark2','$Total' , '' , '')";
+if(mysqli_query($con,$query))
+    echo '<script>alert("Inserted Successfully");document.location="database.php"</script>';
+ } 
+}
+else{
+$query = "INSERT INTO student values('$rollno', '$name','$gender','$mark1','$mark2','$Total' , '' , '')";
+if(mysqli_query($con,$query))
+    echo '<script>alert("Inserted Successfully");document.location="database.php"</script>';
+}
+
+
+?>
